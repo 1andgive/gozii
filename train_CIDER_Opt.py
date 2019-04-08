34fc2ffc888e362d0be71c563f1ac24627ed9d36
Seungjun <seungjun45@kaist.ac.kr>
2019-04-08 22:28:41 +0900

    Cider Opt

--- train_CIDER_Opt.py
+++ train_CIDER_Opt.py
@@ -254,7 +254,7 @@ def main(args):
         # Save the model checkpoints
         model_path = os.path.join(
             args.model_path, args.t_method, 'model{}_LR{}'.format(args.model_num, args.LRdim),
-            'model-{}.pth'.format(epoch + 1))
+            'model-{}-CiderOpt.pth'.format(epoch + 1))
         utils.save_model(model_path, encoder, decoder, epoch, optimizer)
 
 
